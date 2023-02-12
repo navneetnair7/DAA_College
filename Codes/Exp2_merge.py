c7adce20eab4bcd0a5117bf426b3682031b35d42
navneetnair7 <navneetnair95@gmail.com>
2023-02-12 11:44:53 +0530

    Modified experiment 2

--- Codes/Exp2_merge.py
+++ Codes/Exp2_merge.py
@@ -1,7 +1,6 @@
-def merge(a, low, mid, high):
+def merge(a, low, mid, high, count):
     b = [0] * len(a)
     h, i, j = low, low, mid + 1
-    count = 0
     while h <= mid and j <= high:
         if a[h] <= a[j] and j <= high:
             b[i] = a[h]
@@ -20,28 +19,29 @@ def merge(a, low, mid, high):
         for k in range(h, mid+1):
             b[i] = a[k]
             i += 1
-    
+    print("Total number of swaps: ", count)
     for k in range(low, high+1):
         a[k] = b[k]
 
-def mergeSort(a, low, high):
+def mergeSort(a, low, high, count):
     if low < high:
         mid = low + (high - low) // 2
-        mergeSort(a, low, mid)
-        mergeSort(a, mid + 1, high)
-        merge(a, low, mid, high)
-    print("The sorted array is: ", a)
-    
+        mergeSort(a, low, mid, count)
+        mergeSort(a, mid + 1, high, count)
+        merge(a, low, mid, high, count)
+
 a = []
 for i in range(0, 10):
     a.append(50 + 51 * i)
+count = 0
 print("The array which we will use is: ")
 print(a)
 print("\nFor best case: ", a)
-mergeSort(a, 0, 9)
+mergeSort(a, 0, 9, count)
+count = 0
 print("\nFor worst case:", a[::-1])
-mergeSort(a[::-1], 0, 9)
+mergeSort(a[::-1], 0, 9, count)
+count = 0
 print("\nFor random case: ", a[5:7] + a[7:] + a[3:5] + a[:3])
-mergeSort(a[5:7] + a[7:] + a[3:5] + a[:3], 0, 9)
-mergeSort(a, 0, 3)
+mergeSort(a[5:7] + a[7:] + a[3:5] + a[:3], 0, 9, count)
 print(a)
--- Codes/Exp2_quick.py
+++ Codes/Exp2_quick.py
@@ -1,31 +1,37 @@
-def partition(a, low, high):
+def partition(a, low, high, count):
     pivot = a[high]
     i = low - 1
     for j in range(low, high):
         if a[j] <= pivot:
             i += 1
             a[i], a[j] = a[j], a[i]
+            count += 1
     a[i + 1], a[high] = a[high], a[i + 1]
+    count += 1
+    print("Total number of swaps: ", count)
     return i + 1
 
-def quickSort(a, low, high):
+def quickSort(a, low, high, count):
     if low < high:
-        p = partition(a, low, high)
-        quickSort(a, low, p - 1)
-        quickSort(a, p + 1, high)
+        p = partition(a, low, high, count)
+        quickSort(a, low, p - 1, count)
+        quickSort(a, p + 1, high, count)
 
 a = []
 for i in range(10):
     a.append(50 + 51 * i)
+count = 0
 
 print("The array which we will use is: ")
 print(a)
 print("\nFor best case: ", a)
-quickSort(a, 0, 9)
+quickSort(a, 0, 9, count)
+count = 0
 print("Sorted array: ", a)
 print("\nFor worst case:", a[::-1])
-quickSort(a[::-1], 0, 9)
+quickSort(a[::-1], 0, 9, count)
 print("Sorted array: ", a)
+count = 0
 print("\nFor random case: ", a[5:7] + a[7:] + a[3:5] + a[:3])
-quickSort(a[5:7] + a[7:] + a[3:5] + a[:3], 0, 9)
+quickSort(a[5:7] + a[7:] + a[3:5] + a[:3], 0, 9, count)
 print("Sorted array: ", a)
