84e1bd0664ae7c6682a3e79f0670ff8a7c4538b7
navneetnair7 <navneetnair95@gmail.com>
2023-02-14 12:13:25 +0530

    Better

--- Codes/Exp3_Fibo.py
+++ Codes/Exp3_Fibo.py
@@ -4,9 +4,8 @@ def fibo(n):
     else:
         return fibo(n - 1) + fibo(n - 2)
 
-print(fibo(8))
-print(fibo(16))
-print(fibo(32))
-print(fibo(36))
-print(fibo(40))
-
+print("The 8th fibonacci number is: ", fibo(8))
+print("The 16th fibonacci number is: ", fibo(16))
+print("The 32nd fibonacci number is: ", fibo(32))
+print("The 36th fibonacci number is: ", fibo(36))
+print("The 40th fibonacci number is: ", fibo(40))
